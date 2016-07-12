9c32ba6188d8e6b2dfdfb07ab17d34bffc29b4fa
capossito <cassio_caposso@hotmail.com>
2016-07-12 14:05:10 -0500

    setting up screens and start building game logic

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,37 @@ $(document).ready(function() {
 
 	var App = {
 
+		init: function(){
+			App.cacheElements();
+			App.setTimingScreen();
+			App.bindEvents();
+		},
+
+		cacheElements: function(){
+			App.$doc = $(document);
+
+			App.$contentArea = $('.main-content');
+			App.$templateTimingScreen = $('#select-time-template').html();
+			App.$templateGameScreen = $('#game-screen-template').html();
+
+		},
+
+		setTimingScreen: function(){
+			App.$contentArea.html(App.$templateTimingScreen);
+		},
+
+		bindEvents: function(){
+			App.$doc.on('click', '#btn-start-game', App.onStartGamePressed);
+
+		},
+
+
+		//game functions
+
+		onStartGamePressed: function(){
+			App.$contentArea.html(App.$templateGameScreen);
+		}
+
 	};
 
 	App.init();
